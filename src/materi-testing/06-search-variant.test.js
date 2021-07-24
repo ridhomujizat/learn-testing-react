42cf5e1669a844928d855e0b6a6bb52fd6265443
ridhomujizat <ridhomujizat@gmail.com>
2021-07-24 10:44:17 +0700

    testing with get all by

--- src/materi-testing/06-search-variant.test.js
+++ src/materi-testing/06-search-variant.test.js
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-
+import { data } from "./CompVariant";
 describe("Testing semua aplikasi di dalam app", () => {
   it("melakukan testing untuk queryBy dan findBy", async () => {
     render(<App />);
     expect(screen.queryByText(/Selamat datang/)).toBeNull();
-    expect(await screen.findByText(/Selamat Datang/)).toBeInTheDocument();
+    expect(await screen.findByText(/Selamat datang/)).toBeInTheDocument();
+  });
+
+  it("melakukan render sebanyak hari", () => {
+    render(<App />);
+    expect(screen.getAllByTestId("hari")).toHaveLength(data.length);
   });
 });
--- src/materi-testing/CompVariant.js
+++ src/materi-testing/CompVariant.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from "react";
 
+export const data = [
+  "Senin",
+  "Selasa",
+  "Rabu",
+  "Kamis",
+  "Jumat",
+  "Sabtu",
+  "Minggu",
+];
+
 const CompVariant = () => {
   const [user, setUser] = useState(null);
   useEffect(() => {
@@ -7,7 +17,20 @@ const CompVariant = () => {
       setUser("Prawito");
     }, 1000);
   }, []);
-  return <>{user ? <p>Selamat datang {user}</p> : null}</>;
+  return (
+    <>
+      ({user ? <p>Selamat datang {user}</p> : null}
+      <p>Hari-hari yang ada:</p>
+      {data.map((hari, i) => {
+        return (
+          <p data-testid="hari" key={i}>
+            {hari}
+          </p>
+        );
+      })}
+      )
+    </>
+  );
 };
 
 export default CompVariant;
